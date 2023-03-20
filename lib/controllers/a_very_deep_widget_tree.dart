ef9c37a2c1bc625d4c69179cc2f5ceb02c887cf9
jakkarin <jakkarin@buu.ac.th>
2023-03-20 10:05:12 +0700

    .

---
+++ lib/controllers/a_very_deep_widget_tree.dart
@@ -0,0 +1,29 @@
+import 'package:basic_flutter_provider/models/counting_the_number.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ColumnClass extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+// âProvider.ofâ, just like Consumer needs to know the type of the model.
+    // We need to specify the model âCountingTheNumberâ.
+    //this time only this widget will be rebuilt
+    final CountingTheNumber counter = Provider.of<CountingTheNumber>(context);
+    return Column(
+      children: [
+        Text(
+          '${counter.number}',
+          style: TextStyle(fontSize: 25.0),
+        ),
+        SizedBox(height: 10.0),
+        FloatingActionButton(
+          onPressed: () {
+            counter.increaseNumber();
+          },
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:basic_flutter_provider/models/counting_the_number.dart';
+import 'package:basic_flutter_provider/views/my_home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    // ChangeNotifierProvider, unlike ChangeNotifier, comes from the Provider package
+    // and it provides an instance of a ChangeNotifier to the widgets,
+    // which have already subscribed to it
+    // we should place the ChangeNotifierProvider Just above the widgets that need to access it.
+    // you will understand provider better if you already have understood how
+    // InheritedWidget works
+    ChangeNotifierProvider(
+      create: (context) =>
+          CountingTheNumber(), // designed Model is provided to the desired widgets
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
